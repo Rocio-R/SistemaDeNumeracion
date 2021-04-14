4b43befce3aaf2f7d11ffb5251bb2e7505d1c525
Rocio-R <rocioramos2005@yahoo.com.ar>
2021-04-14 17:31:49 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function convert() {
+  let dec= document.getElementById('input').value;
+  let bin = parseInt(dec).toString(2);
+  let oct = parseInt(dec).toString(8);
+  let hex = parseInt(dec).toString(16);
+  document.getElementById('bin').innerHTML =bin;
+  document.getElementById('oct').innerHTML =oct;
+  document.getElementById('hex').innerHTML =hex;
+}
